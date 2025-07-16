fbc43af1886433d7c5869646fb4a3d5d573131a2
1428485959@qq.com <1428485959@qq.com>
2025-07-16 09:31:59 +0800

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,14 @@
-import createMiddleware from "next-intl/middleware"
-import { locales } from "./lib/i18n"
+import createMiddleware from "next-intl/middleware";
+import { locales } from "./lib/i18n";
 
 export default createMiddleware({
   locales,
   defaultLocale: "en",
   localePrefix: "as-needed",
-})
+});
 
 export const config = {
   // Skip API routes, Next.js internals, the Next.js not-found helper,
   // static files, and any path that starts with “_”
-    matcher: ["/((?!api|_next|_vercel|_not-found|_.*|.*\\..*).*)"],
-
-}
+  matcher: ["/((?!api|_next|_vercel|_.*|.*\\..*).*)"],
+};
